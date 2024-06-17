69fbaedda98b9fca480a76b5d983f919c4e55e54
mercury1565 <hermongetachew10@gmail.com>
2024-06-17 11:12:09 +0300

    onChange completed

--- project/DS_frontEnd/src/App.jsx
+++ project/DS_frontEnd/src/App.jsx
@@ -1,11 +1,9 @@
 import MyComponent from "./MyComponent.jsx"; 
-import Counter from "./Counter.jsx";
 
 function App() 
 {
   return(
-    // <MyComponent/>
-    <Counter/>
+    <MyComponent/>
   );
 }
 
--- project/DS_frontEnd/src/MyComponent.jsx
+++ project/DS_frontEnd/src/MyComponent.jsx
@@ -1,34 +1,64 @@
 import React, {useState} from 'react';
 
 function MyComponent(){
-    const [name , setName] = useState('Guest');
-    const [age , setAge] = useState(0);
-    const [isStudent , setBoolean] = useState(false);
+    const [name, setName] = useState("Guest");
+    const [quantity, setQuantity] = useState(0);
+    const [comment , setComment] = useState("");
+    const [payement , setPayement] = useState("");
+    const [shipping , setShipping] = useState("");
 
-    const updateName = () => {
-        setName("Hermon")
+    function handelNameChange(event){
+        setName(event.target.value);
     }
-
-    const incrementAge = () => {
-        setAge(age + 1)
+    function handelQuantityChange(event){
+        setQuantity(event.target.value);
     }
-
-    const toogleBoolean = () => {
-        setBoolean(!isStudent)
+    function handleCommentChange(event){
+        setComment(event.target.value);
     }
-
+    function handlePayementChange(event){
+        setPayement(event.target.value);
+    }
+    function handleShippingChange(event){
+        setShipping(event.target.value);
+    }
 
     return(
-        <div>
-            <p> Name: {name}</p>
-            <button onClick = {updateName}> Set Name </button>
+       <div>
+         <input value = {name}  onChange = {handelNameChange}/>
+         <p> Name: {name} </p>
 
-            <p> Age: {age}</p>
-            <button onClick = {incrementAge}> Increment Age </button>
+         <input value = {quantity}  onChange = {handelQuantityChange} type = "number"/>
+         <p> Quantity: {quantity} </p>
 
-            <p> Is student: {isStudent ? "YES" : "NO"} </p>
-            <button onClick = {toogleBoolean}> Toogle Boolean </button>
-        </div>
+         <textarea value = {comment}  onChange = {handleCommentChange} placeholder='Leave comment'/>
+         <p> Comment: {comment} </p>
+
+         <select value = {payement} onChange = {handlePayementChange}>
+            <option value = ""> Select an option </option>
+            <option value = "Cash"> Cash </option>
+            <option value = "Visa"> Visa </option>
+            <option value = "MasterCard"> MasterCard </option>
+         </select>
+         <p> Payement: {payement} </p>
+
+        <label>
+            <input type = "radio" 
+                    value = "Pick Up"
+                    checked = {shipping === "Pick Up"}
+                    onChange = {handleShippingChange}/>
+            Pick Up
+        </label><br/>
+
+        <label>
+            <input type = "radio" 
+                    value = "Delivery"
+                    checked = {shipping === "Delivery"} 
+                    onChange = {handleShippingChange}/>
+            Delivery
+        </label>
+        <p> Shipping: {shipping} </p>
+       </div>
     );
 
 }
